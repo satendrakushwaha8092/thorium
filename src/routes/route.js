6ada0dfaff4a1a34a7ee57b0e3b38ee34a934de6
satendrakushwha8092 <satykush8092clear screenclear screengit config --global user.email satykush8092clear>
2022-03-10 17:06:32 +0530

    assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const auth= require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/users",userController.createUser)
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId",auth.authenticate,auth.authorise, userController.getUserData)
+//router.post("/users/:userId/posts", userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/updateuser/:userId",auth.authenticate,auth.authorise, userController.updateUser)
+router.delete('/deleteuser/:userId',auth.authenticate,auth.authorise, userController.deleteUser)
+
 
 module.exports = router;
